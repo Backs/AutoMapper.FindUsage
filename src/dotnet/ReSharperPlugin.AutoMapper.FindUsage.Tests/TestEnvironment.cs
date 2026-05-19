51111ee658b7876d39d8114f67f9bcb4818f445f
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2026-05-19 21:13:47 +0700

    initial

---
+++ src/dotnet/ReSharperPlugin.AutoMapper.FindUsage.Tests/TestEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.AutoMapper.FindUsage.Tests
+{
+    [ZoneDefinition]
+    public class AutoMapper.FindUsageTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IAutoMapper.FindUsageZone> { }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<AutoMapper.FindUsageTestEnvironmentZone> { }
+
+    [SetUpFixture]
+    public class AutoMapper.FindUsageTestsAssembly : ExtensionTestEnvironmentAssembly<AutoMapper.FindUsageTestEnvironmentZone> { }
+}
