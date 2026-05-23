8099bc4d8a77a5e1a74bf1f8ac7fc3388fda55b0
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2026-05-23 19:22:20 +0700

    AutoMapperNavigationActionAvailabilityTest

---
+++ src/dotnet/ReSharperPlugin.AutoMapper.FindUsage.Tests/Navigation/AutoMapperNavigationActionAvailabilityTest.cs
@@ -0,0 +1,23 @@
+using JetBrains.ReSharper.FeaturesTestFramework.Intentions;
+using JetBrains.ReSharper.TestFramework;
+using NUnit.Framework;
+using ReSharperPlugin.AutoMapper.FindUsage.Navigation;
+
+namespace ReSharperPlugin.AutoMapper.FindUsage.Tests.Navigation;
+
+[TestFixture]
+[TestPackages("AutoMapper")]
+public class AutoMapperNavigationActionAvailabilityTest
+    : CSharpContextActionAvailabilityTestBase<AutoMapperNavigationAction>
+{
+    protected override string RelativeTestDataPath => "Navigation";
+    protected override string ExtraPath => "";
+
+    [Test] public void TestAvailableOnSetter() => DoNamedTest();
+    
+    [Test] public void TestAvailableOnInit() => DoNamedTest();
+
+    [Test] public void TestNotAvailableOnGetter() => DoNamedTest();
+
+    [Test] public void TestNotAvailableWithoutMapping() => DoNamedTest();
+}
